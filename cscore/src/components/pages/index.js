d6929877170bb8e5b31e5472d85fe0ff52e4385d
Jake Krueger <jmk4u14@gmail.com>
2024-05-27 22:02:11 -0400

    Added forms to home page

--- cscore/src/components/pages/index.js
+++ cscore/src/components/pages/index.js
@@ -1,8 +1,86 @@
 import React from "react";
+import {Row, Container} from "reactstrap";
 
 function Home() {
     return (
-        <p>This is the home page!</p>
+        <Container class="home">
+            <Row>
+                <h1>CScore Level Helper</h1>
+            </Row>
+            <Row>
+                <form>
+                    <label>
+                        Current Base VAPs:
+                        <input type="text" />
+                    </label>
+                </form>
+            </Row>
+            <Row>
+                <form>
+                    <label>
+                        Current Duos:
+                        <input type="text" />
+                    </label>
+                </form>
+            </Row>
+            <Row>
+                <form>
+                    <label>
+                        Current Smarts + Internet:
+                        <input type="text" />
+                    </label>
+                </form>
+            </Row>
+            <Row>
+                <form>
+                    <label>
+                        Current Androids:
+                        <input type="text" />
+                    </label>
+                </form>
+            </Row>
+            <Row>
+                <form>
+                    <label>
+                        Current New Activations:
+                        <input type="text" />
+                    </label>
+                </form>
+            </Row>
+            <Row>
+                <form>
+                    <label>
+                        Current Accessory Comm:
+                        <input type="text" />
+                    </label>
+                </form>
+            </Row>
+            <Row>
+                <form>
+                    <label>
+                        Current Devices:
+                        <input type="text" />
+                    </label>
+                </form>
+            </Row>
+            <Row>
+                <form>
+                    <label>
+                        Current iVAPs:
+                        <input type="text" />
+                    </label>
+                </form>
+            </Row>
+            <Row>
+                <form>
+                    <label>
+                        Current B2B Devices:
+                        <input type="text" />
+                    </label>
+                </form>
+            </Row>
+            <input type="submit" value="Submit" />
+        </Container>
     );
 }
 
